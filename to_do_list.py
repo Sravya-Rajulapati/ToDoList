c7c5e7f2645f6f23eb54a01f2042fe503aaf42bf
Sravya-Rajulapati <rajulapatisravya25@gmail.com>
2023-10-20 22:03:10 +0200

    First Commit

---
+++ to_do_list.py
@@ -0,0 +1,13 @@
+awejyfgawiuhfeirv\re\\
+v\
+fgjkgijgg
+]rgf\r]gorijgojg;'lsa,kd\
+    sd'\fs
+    ;bpv['ojfdsoigjg[aewskgaw
+         \]s
+         [dxp[ojfcvosijgozsjidv
+         ]zsXCV}[\
+            v[]\fgbvlpsdfojvposa
+            v;s\ X"}/ZX:Lvcsjgv
+            s\dv';
+            \dszf'v];lvmnj cnv']]]']'
